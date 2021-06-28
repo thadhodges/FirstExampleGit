9d620c2b5a9eb582215f6f6ff088885cc8bfa100
Thad <thadisfree@gmail.com>
2021-06-27 21:47:15 -0500

    added second question

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!, How are you doing?");
+            Console.WriteLine("What is next in this debate");
         }
     }
 }
