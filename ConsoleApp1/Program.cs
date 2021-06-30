33d235d9994ef56cc01919b67f2c550dd857bcd2
Thad <thadisfree@gmail.com>
2021-06-29 21:15:19 -0500

    added more dialogue

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,10 +6,14 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!, How are you doing?");
-            Console.WriteLine("What is next in this debate");
+            Console.WriteLine("Hello World! Nice to meet you?");
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"{name}, Do you like animals?");
             var next = Console.ReadLine();
-            Console.WriteLine($"Your answer is {next}.");
+            Console.WriteLine($"Your answer is {next}. Well then, {name}, What is your fav animal?");
+            var animal = Console.ReadLine();
+            Console.WriteLine($"Really {name}, I like {animal} too!");
 
         }
     }
